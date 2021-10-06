00c346e5d6e4b214e3665c369a5f57fc468f5698
Manuel Esparza <manoloesparta@gmail.com>
2021-10-05 17:04:27 -0700

    linter

--- server/tests/repos/test_users.py
+++ server/tests/repos/test_users.py
@@ -21,16 +21,17 @@ def mocked_repo():
 
 def test_first_time_login_with_unregistered_user(mocked_repo):
     response = mocked_repo.first_time_login(UNREGISTERED_USER)
-    assert response 
+    assert response
+
 
 def test_first_time_login_with_registered_user(mocked_repo):
     response = mocked_repo.first_time_login(REGISTERED_USER)
-    assert not response 
+    assert not response
 
 
 def test_register_user_with_non_existing_user(mocked_repo):
-    mocked_repo.register_user(UNREGISTERED_USER) 
-    query = { "username": UNREGISTERED_USER.username }
+    mocked_repo.register_user(UNREGISTERED_USER)
+    query = {"username": UNREGISTERED_USER.username}
     user = mocked_repo.db_handler.get_document(query)
     assert user.get("username") == UNREGISTERED_USER.username
     assert user.get("visitCount") == 0
