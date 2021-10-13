414a89fa7fa2859f315dd139f7a6c7546cebdd64
JustGawd <64821152+JustGawd@users.noreply.github.com>
2021-10-12 23:39:39 -0700

    Update on Home.jss , create Home.css
    Co-Authored-By: dagz-git <48745080+dagz-git@users.noreply.github.com>

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../assets/cetys-logo.jpg';
+import './Home.css';
 
 class Home extends React.Component {
   constructor(props) {
@@ -14,7 +15,14 @@ class Home extends React.Component {
         <div>
           <img className="mt-3" src={logo} id="logo"></img>
         </div>
-        <form onSubmit={this.handleSubmit}>
+        <form>
+          <div className="input-group input-group-lg">
+            <input type="text"
+              className="form-control rounded-pill"
+              placeholder="Describe tu objeto..."
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-lg"/>
+          </div>
         </form>
       </div>
     );
