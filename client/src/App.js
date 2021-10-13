2a28fd5d3083b6d23c4889db978755d4156120f8
Manuel Esparza <manoloesparta@gmail.com>
2021-10-13 09:47:51 -0700

    client commits

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-=======
->>>>>>> 5de9bad220ca1d2dea50bed3d99dbf8c73f13ab6
 import './App.css';
 import Login from '../src/pages/Login';
 
