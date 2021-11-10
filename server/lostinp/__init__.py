3056875b8dff72455bb1a155776a8676fe75400b
Manuel Esparza <36689861+manoloesparta@users.noreply.github.com>
2021-11-10 10:07:34 -0800

    Bug fixed auth mocked service in separate container (#39)
    * Authentication Service refactor (#38)
    
    * support authentication for external service added (#37)
    
    * fake auth as separate service
    
    * environments added
    
    * testing sutff

--- server/lostinp/__init__.py
+++ server/lostinp/__init__.py
@@ -11,10 +11,12 @@ def create_app():
     from lostinp.routes.login import bp as login_blueprint
     from lostinp.routes.validate import bp as validate_blueprint
     from lostinp.routes.items import bp as search_items_blueprint
+    from lostinp.routes.health import bp as health_blueprint
 
     app.blueprint(login_blueprint)
     app.blueprint(validate_blueprint)
     app.blueprint(search_items_blueprint)
+    app.blueprint(health_blueprint)
 
     if CONFIG.get_value("ENV") == "dev":
         from lostinp.routes.auth import bp as auth_blueprint
