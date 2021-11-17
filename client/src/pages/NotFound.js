62eab116b89dda2e1351d9ce81fea66b6c2dd561
JustGawd <64821152+JustGawd@users.noreply.github.com>
2021-11-16 22:32:27 -0800

    updated pr

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
+import icon from '../assets/404.png';
 
 function NotFound() {
   return (
@@ -7,9 +8,8 @@ function NotFound() {
       <div className="hero-body has-text-centered">
         <div className="container">
           <div className="column">
-            <img src="/404icon.png"/>
-            <h1 className="title is-1 is-spaced">404 <br/>La pagina no
-            se pudo encontrar!</h1>
+            <img src={icon} width='100px'/>
+            <h1 className="title is-1 is-spaced">LA PAGINA NO EXISTE</h1>
           </div>
         </div>
       </div>
